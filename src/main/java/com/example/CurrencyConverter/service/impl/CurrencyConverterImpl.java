dd5476c8921d47721064590273bd629a8228b115
amanjoshi <ajoshi373@gmail.com>
2024-08-12 00:07:27 +0530

    added logs and made code more generic

--- src/main/java/com/example/CurrencyConverter/service/impl/CurrencyConverterImpl.java
+++ src/main/java/com/example/CurrencyConverter/service/impl/CurrencyConverterImpl.java
@@ -2,6 +2,8 @@ package com.example.CurrencyConverter.service.impl;
 
 import com.example.CurrencyConverter.service.CurrencyConverter;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -12,20 +14,37 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class CurrencyConverterImpl implements CurrencyConverter {
+
+    Logger log = LoggerFactory.getLogger(CurrencyConverterImpl.class);
+
     private final RestClient restClient;
 
     @Override
     public Double convertCurrency(String fromCurrency, String toCurrency, Double units) {
-        String uri="/v1/latest?apikey=fca_live_QXgaZo6KwSL4ymSAXzqt1DBdRZTEOilCGe1HnDuE&currencies=INR";
-        Map<String, Map<String, Double>> currencyExchangeRate=restClient.get()
-                .uri(uri)
+        log.info("Entering CurrencyConverterImpl.convertCurrency()");
+
+        String fromCurrencyUri="/v1/latest?apikey=fca_live_QXgaZo6KwSL4ymSAXzqt1DBdRZTEOilCGe1HnDuE&currencies=INR";
+        Map<String, Map<String, Double>> fromCurrencyExchangeRate=restClient.get()
+                .uri(fromCurrencyUri)
                 .retrieve()
                 .body(new ParameterizedTypeReference<Map<String, Map<String, Double>>>() {
                 });
 
-        Map<String, Double> innerMap=currencyExchangeRate.get("data");
+        String toCurrencyUri="/v1/latest?apikey=fca_live_QXgaZo6KwSL4ymSAXzqt1DBdRZTEOilCGe1HnDuE&currencies="+toCurrency;
+        Map<String, Map<String, Double>> toCurrencyExchangeRate=restClient.get()
+                .uri(toCurrencyUri)
+                .retrieve()
+                .body(new ParameterizedTypeReference<Map<String, Map<String, Double>>>() {
+                });
 
-        return units/(innerMap.get("INR"));
+        Map<String, Double> innerMapFromCurrency=fromCurrencyExchangeRate.get("data");
+        Map<String, Double> innerMapToCurrency=toCurrencyExchangeRate.get("data");
+
+        Double convertedCurrency=units*
+                (innerMapToCurrency.get(toCurrency)/innerMapFromCurrency.get(fromCurrency));
+
+        log.info("Exiting from CurrencyConverterImpl.convertCurrency()");
+        return convertedCurrency;
 
     }
 }
